3d0ac2f2b84ec761386d2a6401b88309c27c5b45
Dheeraj Singh Dani <dheerajdani2@gmail.com>
2022-11-08 22:30:53 +0530

    chnages on multiplication

--- multi.java
+++ multi.java
@@ -2,6 +2,6 @@ public class multi{
   public static void main(String[] args) {
     int a = 3;
     int b = 5;
-    system.out.println("the multiply is = "+ (a/b));
+    system.out.println("the multiply is = "+ (a*b));
   }
 }
