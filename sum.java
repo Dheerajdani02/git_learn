f8400d2e589945a45495f25cfcd956daebf7dad6
Dheeraj Singh Dani <dheerajdani2@gmail.com>
2022-11-09 15:37:43 +0530

    updated sum.java

--- sum.java
+++ sum.java
@@ -1,10 +1,10 @@
 public class sum{
-  public statis void main(String[] args){
+  public static void main(String[] args){
     int a = 5;
     int b = 4;
     int c = 2;
     int d = 9;
 
-    system.out.println("the sum is=" + (a+b));
+    System.out.println("the sum is=" + (a+b));
   }
 }
