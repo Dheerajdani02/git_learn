e18776a5f449738a73464d9145dc24896c0d3395
Dheeraj Singh Dani <dheerajdani2@gmail.com>
2022-11-09 14:36:23 +0530

    add one more multiplication

--- multi.java
+++ multi.java
@@ -2,6 +2,12 @@ public class multi{
   public static void main(String[] args) {
     int a = 3;
     int b = 5;
+    
+    int c = 5;
+    int d = 5;
+
+    system.out.println("the multi is = " + (c*d));
+
     system.out.println("the multiply is = "+ (a*b));
   }
 }
